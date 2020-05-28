2aacd16ef916eb4dfe45e78a0eb8a67a6a1ddda2
姜振祥 <jiangzx@jiedaibao.com>
2020-05-28 18:55:58 +0800

    hash路由参数查询

--- 2020/util.js
+++ 2020/util.js
@@ -212,7 +212,7 @@ export const removeHtmltag = (str) => {
  */
 export const getQueryString = (name) => {
     const reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
-    const search = window.location.search.split('?')[1] || '';
+    const search = window.location.href.split('?')[1] || '';
     const r = search.match(reg) || [];
     return r[2];
 }
